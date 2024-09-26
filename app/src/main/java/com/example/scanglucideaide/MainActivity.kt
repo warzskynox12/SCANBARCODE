519c08a2f83b59ed5574af095b965236e6b61e17
pasnon lucas <107144928+warzskynox12@users.noreply.github.com>
2024-09-27 00:18:03 +0200

    [DEBUG] modification de la detection des codebar et de l'ouverture de l'activiter

--- app/src/main/java/com/example/scanglucideaide/MainActivity.kt
+++ app/src/main/java/com/example/scanglucideaide/MainActivity.kt
@@ -28,17 +28,26 @@ import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import android.os.Handler
+import android.os.Looper
+import kotlin.concurrent.timerTask
+
+private var isProcessingImage = false
 
 @androidx.camera.core.ExperimentalGetImage
 private fun barcodeDetect(context: Context, cameraExecutor: ExecutorService) {
     val barcodeScanner = BarcodeScanning.getClient()
-    // Set up the camera to detect barcodes
     val imageAnalysis = ImageAnalysis.Builder()
         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
         .build()
 
     imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
-        processImageProxy(context, barcodeScanner, imageProxy)
+        if (!isProcessingImage) {
+            isProcessingImage = true
+            processImageProxy(context, barcodeScanner, imageProxy)
+        } else {
+            imageProxy.close()
+        }
     }
 
     val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
@@ -62,40 +71,69 @@ private fun processImageProxy(context: Context, scanner: BarcodeScanner, imagePr
     val mediaImage = imageProxy.image
     if (mediaImage != null) {
         val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+        var codeBar: String? = null
+        val handler = Handler(Looper.getMainLooper())
+        val timeoutRunnable = Runnable {
+            if (isProcessingImage) {
+                imageProxy.close()
+                isProcessingImage = false
+                reloadCamera(context)
+            }
+        }
+        handler.postDelayed(timeoutRunnable, 2000) // 2 seconds timeout
+
         scanner.process(image)
             .addOnSuccessListener { barcodes ->
                 for (barcode in barcodes) {
-                    // Handle the detected barcode
-                    val codeBar = barcode.rawValue
-                    if (codeBar != null) openScreen(context, codeBar)
+                    codeBar = barcode.rawValue
                 }
             }
             .addOnFailureListener {
-                // Handle failure
+                reloadCamera(context)
             }
             .addOnCompleteListener {
+                handler.removeCallbacks(timeoutRunnable)
                 imageProxy.close()
+                isProcessingImage = false
+                codeBar?.let {
+                    openScreen(context, it)
+                } ?: reloadCamera(context)
             }
+    } else {
+        imageProxy.close()
+        isProcessingImage = false
+        reloadCamera(context)
     }
 }
-private fun openScreen(context: Context, codeBar: String) {
-    val intent = Intent(context, SecondActivity::class.java)
-    intent.putExtra("codeBar", codeBar)
-    startActivity(context, intent, null)
+
+@OptIn(ExperimentalGetImage::class)
+private fun reloadCamera(context: Context) {
+    startActivity(context, Intent(context, MainActivity::class.java), null)
+    Toast.makeText(context, "Failed to detect barcode", Toast.LENGTH_SHORT).show()
 }
 
-private fun showBarcodeInputDialog(context: Context) {
+private var isScreenOpened = false
+
+private fun openScreen(context: Context, codeBar: String) {
+    if (!isScreenOpened) {
+        isScreenOpened = true
+        val intent = Intent(context, SecondActivity::class.java)
+        intent.putExtra("codeBar", codeBar)
+        startActivity(context, intent, null)
+        if (context is MainActivity) {
+            context.finish()
+        }
+    }
+}
+    private fun showBarcodeInputDialog(context: Context) {
     val builder = AlertDialog.Builder(context)
     builder.setTitle("Enter Barcode")
 
-    // Set up the input
     val input = EditText(context)
     builder.setView(input)
 
-    // Set up the buttons
     builder.setPositiveButton("OK") { dialog, _ ->
         val barcode = input.text.toString()
-        // Handle the barcode input
         Toast.makeText(context, "Barcode entered: $barcode", Toast.LENGTH_SHORT).show()
         dialog.dismiss()
     }
@@ -105,13 +143,13 @@ private fun showBarcodeInputDialog(context: Context) {
 
     builder.show()
 }
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraPreview: PreviewView
     private lateinit var cameraExecutor: ExecutorService
     private var camera: Camera? = null
 
-    // Permission request for CameraX
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
@@ -126,13 +164,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize Camera preview
         cameraPreview = findViewById(R.id.camera_preview)
-
-        // Initialize camera executor for background tasks
         cameraExecutor = Executors.newSingleThreadExecutor()
 
-        // Check camera permission and start camera
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
             PackageManager.PERMISSION_GRANTED) {
             startCamera()
@@ -140,20 +174,17 @@ class MainActivity : AppCompatActivity() {
             requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
 
-        // Button for automatic scan
         val scanButton = findViewById<Button>(R.id.button)
         scanButton.setOnClickListener {
             barcodeDetect(this, cameraExecutor)
         }
 
-        // Button for manual scan
         val manualScanButton = findViewById<Button>(R.id.button1)
         manualScanButton.setOnClickListener {
             showBarcodeInputDialog(this)
         }
     }
 
-    // Start camera using CameraX
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
--- app/src/main/java/com/example/scanglucideaide/SecondActivity.kt
+++ app/src/main/java/com/example/scanglucideaide/SecondActivity.kt
@@ -1,13 +1,34 @@
 // Ensure SecondActivity extends AppCompatActivity or Activity
 package com.example.scanglucideaide
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ExperimentalGetImage
+import androidx.core.content.ContextCompat
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val codeBar = intent.getStringExtra("codeBar")
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = codeBar
+        val manualScanButton = findViewById<Button>(R.id.button_return)
+        manualScanButton.setOnClickListener {
+            returnScan(this)
+        }
     }
+
+    private fun returnScan(context: Context) {
+        val intent = Intent(context, MainActivity::class.java)
+        ContextCompat.startActivity(context, intent, null)
+    }
+
 }
